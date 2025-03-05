3a0a3bacf018940407fc79c743e08b76fb34260c
s-light <git@s-light.eu>
2025-03-05 10:19:59 +0100

    basic example setup done

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,26 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import Markdown from "unplugin-vue-markdown/vite";
+
+import prism from "markdown-it-prism";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      include: [/\.vue$/, /\.md$/], // <-- allows Vue to compile Markdown files
+    }),
+    Markdown({
+      markdownItUses: [
+        prism,
+      ],
+    }),
     vueDevTools(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
